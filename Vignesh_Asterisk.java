807a5a2eb007cfa9e8aa9259e6410dd79a19413a
vignesh-parthiban <84707720+vignesh-parthiban@users.noreply.github.com>
2021-05-31 18:57:41 +0530

    Add files via upload

---
+++ Vignesh_Asterisk.java
@@ -0,0 +1,38 @@
+class Vignesh_Asterisk   
+{   
+public static void main(String args[])   
+{   
+
+int i, j;  
+
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<1; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}
+ 
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<2; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}   
+
+ 
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<3; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}
+
+   
+} 
+}  
---
+++ Vignesh_Asterisk_Reverse.java
@@ -0,0 +1,42 @@
+class Vignesh_Asterisk_Reverse   
+{   
+public static void main(String args[])   
+{   
+
+int i, j;  
+
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<3; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}
+
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<2; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}
+
+for(i=0; i<1; i++)   
+{   
+for(j=0; j<1; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}
+ 
+   
+
+ 
+
+
+   
+} 
+}  
